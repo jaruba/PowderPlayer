36b8cf801559bdd1612e709d51082b5bbac92358
jaruba <branza.alex@gmail.com>
2015-05-04 09:08:16 +0300

    Updated Version

--- engine.js
+++ engine.js
@@ -16,7 +16,7 @@
 * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
 *****************************************************************************/
 
-localStorage.powderVersion = "0.17";
+localStorage.powderVersion = "0.18";
 
 var checkedUpdates = 0;
 
