00e5b28251f60fcaac2fd103edae243984a81d90
Luigi Poole <luigipoole@outlook.com>
2015-11-11 01:18:33 -0800

    add Player route

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import Framework from './components/Framework.react';
 import Dashboard from './components/Dashboard.react';
 import Preferences from './components/Preferences.react';
 import About from './components/About.react';
+import Player from './components/Player';
 
 
 export
@@ -16,5 +17,6 @@ default (
       <IndexRoute component={Dashboard}/>
       <Route path="/preferences" component={Preferences}/>
       <Route path="/about" component={About}/>
+      <Route path="/player" component={Player}/>
     </Route>
 );
