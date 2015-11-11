f3d0e47155050a2c82af50ba972bffe62552ae21
Luigi Poole <luigipoole@outlook.com>
2015-11-11 00:57:41 -0800

    more work on torrent util

--- src/utils/torrentUtil.js
+++ src/utils/torrentUtil.js
@@ -1,15 +1,33 @@
+import peerflix from 'peerflix';
+import path from 'path';
+import readTorrent from 'read-torrent';
+import Promise from 'bluebird';
+import getPort from 'get-port';
+
+
 export
 default {
     streams: [],
 
-
     init: function(torrent) {
+        Promise.all([this.parse(torrent), getPort()])
+            .spread((torrentInfo, port) => {
 
-
-
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     },
     preload: function(torrent) {
 
     },
+    parse: function(torrent) {
+        return new Promise((resolve, reject) => {
+            readTorrent(torrent, (err, torrent) => {
+                return (err) ? reject(err) : resolve(torrent);
+            });
+        });
+    },
+}
 
 };
