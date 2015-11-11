3a2bc8b83641c1354d76b45781fed4e60bf84a59
Luigi Poole <luigipoole@outlook.com>
2015-11-11 00:40:20 -0800

    start of new torrentUtil

--- src/utils/torrentUtil.js
+++ src/utils/torrentUtil.js
@@ -1,30 +1,15 @@
-var WebUtil = {
-    addLiveReload: function() {
-        var head = document.getElementsByTagName('head')[0];
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.src = 'http://localhost:35729/livereload.js';
-        head.appendChild(script);
-    },
-    disableGlobalBackspace: function() {
-        document.onkeydown = function(e) {
-            e = e || window.event;
-            var doPrevent;
-            if (e.keyCode === 8) {
-                var d = e.srcElement || e.target;
-                if (d.tagName.toUpperCase() === 'INPUT' || d.tagName.toUpperCase() === 'TEXTAREA') {
-                    doPrevent = d.readOnly || d.disabled;
-                } else {
-                    doPrevent = true;
-                }
-            } else {
-                doPrevent = false;
-            }
-            if (doPrevent) {
-                e.preventDefault();
-            }
-        };
-    },
-};
+export
+default {
+    streams: [],
 
-module.exports = WebUtil;
+
+    init: function(torrent) {
+
+
+
+    },
+    preload: function(torrent) {
+
+    },
+
+};
