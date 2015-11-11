1b583888519c97d29d7d6b19fd3badd9ae7c2519
Luigi Poole <luigipoole@outlook.com>
2015-11-11 01:16:39 -0800

    Add player component

---
+++ src/components/Player/Header.react.js
@@ -0,0 +1,30 @@
+﻿import React from 'react';
+import PlayerHeader from './Header.react';
+
+
+export
+default React.createClass({
+    getInitialState() {
+        return {
+
+        }
+    },
+    componentDidMount() {
+
+    },
+    componentWillUnmount() {
+
+    },
+    update() {
+
+    },
+
+    render() {
+        return (
+            <div >
+               <PlayerHeader/>
+               
+            </div>
+        );
+    }
+});
---
+++ src/components/Player/index.js
@@ -0,0 +1,26 @@
+﻿import React from 'react';
+
+export
+default React.createClass({
+    getInitialState() {
+        return {
+
+        }
+    },
+    componentDidMount() {
+
+    },
+    componentWillUnmount() {
+
+    },
+    update() {
+
+    },
+    render() {
+        return (
+            <div >
+               
+            </div>
+        );
+    }
+});
