59e0d49dc0614463f6220c5d3cb2d18697d51884
Luigi Poole <luigipoole@outlook.com>
2015-11-12 02:36:49 -0800

    Do stuff on URL submit!

--- src/components/Modal/components/URLadd.js
+++ src/components/Modal/components/URLadd.js
@@ -1,21 +1,30 @@
 import React from 'react';
 
+import utils from '../../../utils/util';
 
 export
 default React.createClass({
+
+    handelURLAdd() {
+        if (this.refs.urlInput.value.length > 0) {
+            console.log('value:', this.refs.urlInput.value)
+            console.log('detected:', utils.parseURL(this.refs.urlInput.value))
+        }
+    },
+
     render() {
         return (
             <div className="holdCenter">
  				<div id="formHolder">
-                        <form className="pure-form pure-form-aligned" onsubmit="urlFormAction(); return false;" id="magnetForm">
-                            <fieldset>
-                                <div className="pure-control-group">
-                                    <input id="magnetLink" type="text" name="magnet" placeholder="Magnet/Torrent URI or Video URL" />
-                                    <button id="magnetSubmit" type="submit" className="pure-button pure-button-primary">Stream</button>
-                                </div>
-                            </fieldset>
-                        </form>
-                    </div>
+                    <form className="pure-form pure-form-aligned" onSubmit={this.handelURLAdd} >
+                        <fieldset>
+                            <div className="pure-control-group">
+                                <input ref="urlInput" type="text" placeholder="Magnet/Torrent URI or Video URL" />
+                                <button type="submit" className="pure-button pure-button-primary">Stream</button>
+                            </div>
+                        </fieldset>
+                    </form>
+                </div>
       		</div>
         );
     }
