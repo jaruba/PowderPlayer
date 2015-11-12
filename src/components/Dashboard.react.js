37298ca655639a7024b9bceae45483a894e7851e
Luigi Poole <luigipoole@outlook.com>
2015-11-12 01:14:47 -0800

    onclicks for buttions

--- src/components/Dashboard.react.js
+++ src/components/Dashboard.react.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import utils from '../utils/util';
 
-
 let If = React.createClass({
     render() {
         return this.props.test ? this.props.children : false;
@@ -27,6 +26,19 @@ default React.createClass({
             this.setState(state);
         }
     },
+
+    addSource(source) {
+        switch (source) {
+            case 'magnet':
+
+
+                break;
+
+            default:
+
+        }
+    },
+
     render() {
         return (
             <div className="wrapper">
@@ -42,17 +54,17 @@ default React.createClass({
                         <br/>
                         <br/>
                         <div className="mainButHold">
-                        	<div className="mainButtons goLeft torrentBut">
+                        	<div onClick={this.addSource.bind(this, 'torrent')} className="mainButtons goLeft torrentBut">
 								<img className="torIcon" src="images/icons/torrent-icon.png"/>
 								<br/>
 								<p>Add Torrent</p>
 		                    </div>
-		                    <div className="mainButtons videoBut">
+		                    <div onClick={this.addSource.bind(this, 'localFile')} className="mainButtons videoBut">
                     			<img className="vidIcon" src="images/icons/video-icon.png"/>
                     			<br/>
                     			<p>Add Video</p>
                     		</div>
-                    		<div className="mainButtons goRight noMarginRight linkBut">
+                    		<div onClick={this.addSource.bind(this, 'magnet')} className="mainButtons goRight noMarginRight linkBut">
 			                    <img className="urlIcon" src="images/icons/link-icon.png"/>
 			                    <br/>
 			                    <p>Use a URL</p>
