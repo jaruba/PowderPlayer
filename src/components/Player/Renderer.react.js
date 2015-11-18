55d997022a8bbe89ed5a4db824a473e614087c5b
Luigi Poole <luigipoole@outlook.com>
2015-11-17 23:24:53 -0800

    Get player to you know.. play.

--- src/components/Player/Renderer.react.js
+++ src/components/Player/Renderer.react.js
@@ -1,8 +1,6 @@
 ﻿import React from 'react';
 import path from 'path';
-
 import wcjsRenderer from './utils/wcjs-renderer';
-
 
 try {
     var wcjs = require(path.join(process.cwd(), 'resources/bin/', 'WebChimera.js.node'));
@@ -29,23 +27,45 @@ export
 default React.createClass({
     getInitialState() {
         return {
-
+            uri: (this.props.uri) ? this.props.uri : ''
         }
     },
     componentDidMount() {
-        console.log('mounting Player')
-        wcjsRenderer.init(wcjs, this.refs['wcjs-render'], wcjsVlcArgs);
+        this.player = wcjsRenderer.init(wcjs, this.refs['wcjs-render'], wcjsVlcArgs);
+        this.player.play("http://archive.org/download/CartoonClassics/Krazy_Kat_-_Keeping_Up_With_Krazy.mp4");
+        window.addEventListener('resize', this.handleResize);
+        this.handleResize();
     },
     componentWillUnmount() {
-
+        window.removeEventListener('resize', this.handleResize);
     },
     update() {
 
     },
+    handleResize() {
 
+        var canvas = this.refs['wcjs-render'];
+        var container = document.body;
+        var sourceAspect = canvas.width / canvas.height;
+        var destAspect = container.clientWidth / container.clientHeight;
+        var cond = destAspect > sourceAspect;
+
+        if (cond) {
+            canvas.style.height = "100%";
+            canvas.style.width = ((container.clientHeight * sourceAspect) / container.clientWidth) * 100 + "%";
+        } else {
+            canvas.style.height = ((container.clientWidth / sourceAspect) / container.clientHeight) * 100 + "%";
+            canvas.style.width = "100%";
+        };
+
+    },
+    handleTogglePlay(toggle) {
+        if (!this.player && (typeof(state) !== undefined && ((this.player.state === 4) == state))) return;
+        this.player.togglePause(toggle ? true : false);
+    },
     render() {
         return (
-            <div >
+            <div>
                <canvas ref="wcjs-render"/>
             </div>
         );
