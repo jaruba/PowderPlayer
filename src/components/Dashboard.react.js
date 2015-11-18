fe61e3da62481410c5d2e39e05028ebe3eb3e27c
Luigi Poole <luigipoole@outlook.com>
2015-11-18 02:10:33 -0800

    add local video file trigger

--- src/components/Dashboard.react.js
+++ src/components/Dashboard.react.js
@@ -11,25 +11,6 @@ import ModalActions from './Modal/actions';
 
 export
 default React.createClass({
-    getInitialState() {
-        return {
-
-        };
-    },
-    componentDidMount() {
-
-    },
-    componentWillUnmount() {
-
-    },
-    update() {
-        if (this.isMounted()) {
-            this.setState({
-
-            });
-        }
-    },
-
     addSource(source) {
         switch (source) {
             case 'url':
@@ -38,22 +19,26 @@ default React.createClass({
                     type: 'URLAdd'
                 });
                 break;
+            case 'local-video':
+                var filters = [{
+                    name: 'Video Files',
+                    extensions: ['MP4', 'MKV', 'MOV', 'AVI', 'WMV', 'WMA', 'ASF', '3GP', 'OGM', 'OGG', 'WAV', 'Real']
+                }];
             case 'local-torrent':
+                var filters = filters ? filters : [{
+                    name: 'Torrents',
+                    extensions: ['TORRENT', 'MAGNET']
+                }];
                 remote.require('dialog')
                     .showOpenDialog({
                         title: 'Select file',
                         properties: ['openFile', 'createDirectory'],
-                        filters: [{
-                            name: 'Torrents',
-                            extensions: ['torrent', 'magnet']
-                        }]
+                        filters: filters
                     }, (filename) => {
                         console.log(filename)
                     });
-            default:
         }
     },
-
     render() {
         return (
             <div className="wrapper">
@@ -70,7 +55,7 @@ default React.createClass({
                         <br/>
                         <div className="mainButHold">
                             <RaisedButton style={{float: 'left', width: '130px'}} onClick={this.addSource.bind(this, 'local-torrent')} label="Add Torrent" />
-                            <RaisedButton style={{width: '130px'}} label="Add Video" />
+                            <RaisedButton style={{width: '130px'}} onClick={this.addSource.bind(this, 'local-video')} label="Add Video" />
                             <RaisedButton style={{float: 'right', width: '130px'}} onClick={this.addSource.bind(this, 'url')} label="Use a URL" />
                         </div>
                     </div>
