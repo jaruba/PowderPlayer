ba0fb5405530e7299b21b9c993c841112ccba8cd
Luigi Poole <luigipoole@outlook.com>
2015-11-22 17:52:45 -0800

    Ideas

--- src/components/TorrentDashboard.react.js
+++ src/components/TorrentDashboard.react.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import {
+    Card, CardActions, CardExpandable, CardHeader, CardText
+}
+from 'material-ui';
 
 import engineStore from '../stores/engineStore';
 import torrentActions from '../actions/torrentActions';
@@ -25,12 +29,23 @@ default React.createClass({
             });
         }
     },
-
-
     render() {
         return (
             <div className="wrapper">
-             
+             	<Card initiallyExpanded={true}>
+  					<CardHeader
+    					title="Name of Torrent"
+    					subtitle="Speed Up/Down"
+    					actAsExpander={true}
+    					showExpandableButton={true}>
+  					</CardHeader>
+  					<CardText expandable={true}>
+    					List of files with progress here
+  					</CardText>
+  					<CardActions expandable={true}>
+    					<FlatButton label="Remove Torrent"/>
+  					</CardActions>
+				</Card>
             </div>
         );
     }
