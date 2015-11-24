84a144df6561e52ba86a6ddacb02824f8da88249
Luigi Poole <luigipoole@outlook.com>
2015-11-23 23:01:46 -0800

    work on playlist some more

--- src/components/Player/components/Playlist.react.js
+++ src/components/Player/components/Playlist.react.js
@@ -1,20 +1,34 @@
 ﻿import React from 'react';
+import _ from 'lodash';
 import {
-    IconButton
+    IconButton, Paper
 }
 from 'material-ui';
-
 import PlayerStore from '../store';
 import PlayerActions from '../actions';
 
+
+
+
+const PlaylistItem = React.createClass({
+    render() {
+        return (
+            <Paper className="item" zDepth={1}>
+                <img src={this.props.image}/>
+                <p className="title">{this.props.title}</p>
+            </Paper>
+        );
+    }
+});
 
 export
 default React.createClass({
     getInitialState() {
         return {
-            open: false
+            open: false,
         }
     },
+
     componentWillMount() {
         PlayerStore.listen(this.update);
     },
@@ -31,8 +45,17 @@ default React.createClass({
 
     },
     render() {
+     
+
         return (
             <div className={this.state.open ? 'playlist-container show' : 'playlist-container'}>
+                <div className="playlist-inner">
+                    
+                    <PlaylistItem
+                        image="https://walter.trakt.us/images/episodes/001/987/912/screenshots/original/c9596bfbc7.jpg" 
+                        title="Always Accountable" />
+
+                </div>
             
             </div>
         );
