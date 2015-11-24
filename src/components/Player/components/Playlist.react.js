1415e447e579a2dfb36eeb002ec434623ede99ad
Luigi Poole <luigipoole@outlook.com>
2015-11-23 17:59:03 -0800

    begin working on playlist component

---
+++ src/components/Player/components/Playlist.react.js
@@ -0,0 +1,40 @@
+﻿import React from 'react';
+import {
+    IconButton
+}
+from 'material-ui';
+
+import PlayerStore from '../store';
+import PlayerActions from '../actions';
+
+
+export
+default React.createClass({
+    getInitialState() {
+        return {
+            open: false
+        }
+    },
+    componentWillMount() {
+        PlayerStore.listen(this.update);
+    },
+    componentWillUnmount() {
+        PlayerStore.unlisten(this.update);
+    },
+    update() {
+        if (this.isMounted()) {
+            this.setState({});
+        }
+    },
+    handleOpenPlaylist() {
+
+
+    },
+    render() {
+        return (
+            <div className={this.state.open ? 'playlist-container show' : 'playlist-container'}>
+            
+            </div>
+        );
+    }
+});
