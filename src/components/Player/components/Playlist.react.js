b1c4c3600852753fe7023fe0344df1a0f51c7c0d
Luigi Poole <luigipoole@outlook.com>
2015-11-24 00:03:43 -0800

    work on playlist

--- src/components/Player/components/Playlist.react.js
+++ src/components/Player/components/Playlist.react.js
@@ -1,6 +1,10 @@
 ﻿import React from 'react';
 import _ from 'lodash';
 import {
+    ItemMixin, ListMixin
+}
+from '../utils/playlistGrid';
+import {
     IconButton, Paper
 }
 from 'material-ui';
@@ -8,9 +12,8 @@ import PlayerStore from '../store';
 import PlayerActions from '../actions';
 
 
-
-
 const PlaylistItem = React.createClass({
+    mixins: [ItemMixin],
     render() {
         return (
             <Paper className="item" zDepth={1}>
@@ -23,14 +26,35 @@ const PlaylistItem = React.createClass({
 
 export
 default React.createClass({
+    mixins: [ListMixin],
     getInitialState() {
         return {
             open: false,
         }
     },
-
     componentWillMount() {
         PlayerStore.listen(this.update);
+    },
+    componentDidMount() {
+        this.setState({
+            items: [{
+                image: 'https://walter.trakt.us/images/episodes/001/987/912/screenshots/original/c9596bfbc7.jpg',
+                title: 'Always Accountable 1',
+                id: 1
+            }, {
+                image: 'https://walter.trakt.us/images/episodes/001/987/912/screenshots/original/c9596bfbc7.jpg',
+                title: 'Always Accountables 2',
+                id: 2
+            }, {
+                image: 'https://walter.trakt.us/images/episodes/001/987/912/screenshots/original/c9596bfbc7.jpg',
+                title: 'Always Accountables 3',
+                id: 3
+            }, {
+                image: 'https://walter.trakt.us/images/episodes/001/987/912/screenshots/original/c9596bfbc7.jpg',
+                title: 'Always Accountables 4',
+                id: 4
+            }]
+        });
     },
     componentWillUnmount() {
         PlayerStore.unlisten(this.update);
@@ -44,17 +68,23 @@ default React.createClass({
 
 
     },
+    handleSort(reorder) {
+        this.setState({
+            items: reorder.map(function(idx) {
+                return this.state.items[idx];
+            }.bind(this))
+        });
+    },
     render() {
-     
-
+        console.log(this.state)
         return (
             <div className={this.state.open ? 'playlist-container show' : 'playlist-container'}>
                 <div className="playlist-inner">
-                    
-                    <PlaylistItem
-                        image="https://walter.trakt.us/images/episodes/001/987/912/screenshots/original/c9596bfbc7.jpg" 
-                        title="Always Accountable" />
-
+                    {
+                        this.state.items.map(function(item, idx) {
+                            return <PlaylistItem key={idx} index={item.id} {...this.movableProps} image={item.image} title={item.title} />;
+                        }, this)
+                    }
                 </div>
             
             </div>
