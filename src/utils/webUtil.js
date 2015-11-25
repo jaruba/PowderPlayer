caa782c556c4f31ecb486d0f95b4cd8d2660208c
Luigi Poole <luigipoole@outlook.com>
2015-11-24 22:53:00 -0800

    es6 webUtil

--- src/utils/webUtil.js
+++ src/utils/webUtil.js
@@ -1,29 +1,26 @@
 export
 default {
-    addLiveReload: function() {
+    addLiveReload() {
         var head = document.getElementsByTagName('head')[0];
         var script = document.createElement('script');
         script.type = 'text/javascript';
         script.src = 'http://localhost:35729/livereload.js';
         head.appendChild(script);
     },
-    disableGlobalBackspace: function() {
-        document.onkeydown = function(e) {
+    disableGlobalBackspace() {
+        document.onkeydown = (e) => {
             e = e || window.event;
             var doPrevent;
             if (e.keyCode === 8) {
                 var d = e.srcElement || e.target;
-                if (d.tagName.toUpperCase() === 'INPUT' || d.tagName.toUpperCase() === 'TEXTAREA') {
+                if (d.tagName.toUpperCase() === 'INPUT' || d.tagName.toUpperCase() === 'TEXTAREA')
                     doPrevent = d.readOnly || d.disabled;
-                } else {
+                else
                     doPrevent = true;
-                }
-            } else {
+            } else
                 doPrevent = false;
-            }
-            if (doPrevent) {
+            if (doPrevent)
                 e.preventDefault();
-            }
         };
     },
 };
