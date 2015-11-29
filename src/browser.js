37ae1d41390c2b690a6fa694211fcea628cc2b7c
Luigi Poole <luigipoole@outlook.com>
2015-11-29 00:57:43 -0800

    i dont plan on talking to powder? (disable speech-api)

--- src/browser.js
+++ src/browser.js
@@ -30,6 +30,10 @@ app.commandLine.appendSwitch('vmodule', 'console=0');
 app.commandLine.appendSwitch('d3d9');
 app.commandLine.appendSwitch('disable-d3d11');
 app.commandLine.appendSwitch('gpu-no-context-lost');
+
+app.commandLine.appendSwitch('disable-speech-api');
+
+
 //app.commandLine.appendSwitch('ignore-gpu-blacklist');
 
 app.on('ready', () => {
