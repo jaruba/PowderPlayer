3e8409432266e8d535b09ce842b0bb0e153b5ba6
Luigi Poole <luigipoole@outlook.com>
2016-01-01 00:18:28 -0800

    suported links to es6 class

--- src/components/Player/utils/supportedLinks.js
+++ src/components/Player/utils/supportedLinks.js
@@ -1,165 +1,166 @@
 import MetaInspector from 'node-metainspector';
+import Promice from 'bluebird';
 import PlayerActions from '../actions';
 
-var linker = {};
 
-linker.isSupportedLink = link => {
+class supportedLinks {
+    isSupportedLink(link) {
 
-    if (link.indexOf('youtube.com/watch?v=') > -1) {
-        return true;
-    }
-
-    if (link.indexOf('vimeo.com/') > -1) {
-
-        var simpler = link.substr(link.indexOf('vimeo.com/')+10);
-
-        if (simpler.indexOf('?') > -1) {
-            simpler = simpler.substr(0, simpler.indexOf('?'));
+        if (link.includes('youtube.com/watch?v=')) {
+            return true;
         }
 
-        if (simpler.indexOf('#') > -1) {
-            simpler = simpler.substr(0, simpler.indexOf('#'));
-        }
+        if (link.includes('vimeo.com/')) {
 
-        if (!isNaN(simpler)) return true;
+            var simpler = link.substr(link.includes('vimeo.com/'));
 
-    }
-
-    if (link.indexOf('soundcloud.com') > -1) {
-
-        if (link.indexOf('?q=') > -1) return false;
-
-        if (link.indexOf('/search/') > -1) return false;
-
-        if (link.indexOf('/groups/') > -1) return false;
-
-        if (link.indexOf('/popular/') > -1) return false;
-
-        if ((link.match(/\//g) || []).length == 4) return true;
-
-        if ((link.match(/\//g) || []).length == 5 && link.indexOf('/sets/')) return true;
-
-    }
-
-    return false;
-};
-
-linker.iframeToLink = el => {
-
-    if (el.indexOf('youtube.com/embed/') > -1) {
-        el = el.substr(el.indexOf('youtube.com/embed/')+18);
-        if (el.indexOf('?') > -1) {
-            el = el.substr(0,el.indexOf('?'));
-        }
-        el = 'https://www.youtube.com/watch?v='+el;
-    }
-
-    if (el.indexOf('player.vimeo.com/video/') > -1) {
-        el = el.substr(el.indexOf('player.vimeo.com/video')+23);
-        if (el.indexOf('?') > -1) {
-            el = el.substr(0,el.indexOf('?'));
-        }
-        el = 'https://vimeo.com/'+el;
-    }
-
-    return el;
-
-};
-
-linker.handleURL = (parsed, cb) => {
-//    console.log(parsed.url);
-    if (!linker.isSupportedLink(parsed.url)) {
-
-        var client = new MetaInspector(parsed.url, { timeout: 5000 });
-
-        client.on("fetch", function(){
-
-//            console.log(client);
-
-            var newFiles = [];
-            var noDupes = [];
-            var links = [];
-
-            if (client.links.absolute) {
-                if (client.links.absolute.http && client.links.absolute.https) {
-                    links = client.links.absolute.http.concat(client.links.absolute.https);
-                } else if (client.links.absolute.http) {
-                    links = client.links.absolute.http;
-                } else if (client.links.absolute.https) {
-                    links = client.links.absolute.https;
-                }
+            if (simpler.includes('?')) {
+                simpler = simpler.substr(0, simpler.includes('?'));
             }
 
-            var iframes = client.iframes;
+            if (simpler.includes('#')) {
+                simpler = simpler.substr(0, simpler.includes('#'));
+            }
 
-            var queueParser = [];
+            if (!isNaN(simpler)) return true;
 
-            links.forEach( (el, ij) => {
-                if (noDupes.indexOf(el) == -1 && linker.isSupportedLink(el)) {
-                    queueParser.push({
-                        idx: ij,
-                        url: el
+        }
+
+        if (link.includes('soundcloud.com')) {
+
+            if (link.includes('?q=')) return false;
+
+            if (link.includes('/search/')) return false;
+
+            if (link.includes('/groups/')) return false;
+
+            if (link.includes('/popular/')) return false;
+
+            if ((link.match(/\//g) || []).length == 4) return true;
+
+            if ((link.match(/\//g) || []).length == 5 && link.includes('/sets/')) return true;
+
+        }
+
+        return false;
+    }
+
+
+    iframeToLink(el) {
+
+        if (el.includes('youtube.com/embed/')) {
+            el = el.substr(el.indexOf('youtube.com/embed/') + 18);
+            if (el.includes('?')) {
+                el = el.substr(0, el.includes('?'));
+            }
+            el = 'https://www.youtube.com/watch?v=' + el;
+        }
+
+        if (el.includes('player.vimeo.com/video/')) {
+            el = el.substr(el.indexOf('player.vimeo.com/video') + 23);
+            if (el.includes('?')) {
+                el = el.substr(0, el.includes('?'));
+            }
+            el = 'https://vimeo.com/' + el;
+        }
+
+        return el;
+    }
+
+
+    handleURL(parsed) {
+        return new Promise((resolve, reject) => {
+            if (!linker.isSupportedLink(parsed.url)) {
+
+                var client = new MetaInspector(parsed.url, {
+                    timeout: 5000
+                });
+
+                client.on("fetch", () => {
+                    var newFiles = [];
+                    var noDupes = [];
+                    var links = [];
+
+                    if (client.links.absolute) {
+                        if (client.links.absolute.http && client.links.absolute.https) {
+                            links = client.links.absolute.http.concat(client.links.absolute.https);
+                        } else if (client.links.absolute.http) {
+                            links = client.links.absolute.http;
+                        } else if (client.links.absolute.https) {
+                            links = client.links.absolute.https;
+                        }
+                    }
+
+                    var iframes = client.iframes;
+
+                    var queueParser = [];
+
+                    links.forEach((el, ij) => {
+                        if (noDupes.indexOf(el) == -1 && linker.isSupportedLink(el)) {
+                            queueParser.push({
+                                idx: ij,
+                                url: el
+                            });
+                            newFiles.push({
+                                uri: el
+                            });
+                        }
+                        noDupes.push(el);
                     });
-                    newFiles.push({
-                        uri: el
-                    });
-                }
-                noDupes.push(el);
-            });
 
-            client.host = client.host.replace('www.','');
+                    client.host = client.host.replace('www.', '');
 
-            if (['soundcloud.com', 'youtube.com', 'vimeo.com'].indexOf(client.host) > -1) {
-                var relatives = client.links.relative;
-                relatives.forEach( (el, ij) => {
-                    el = 'https://'+client.host+el;
-//                    console.log(el);
-                    if (noDupes.indexOf(el) == -1 && linker.isSupportedLink(el)) {
-//                        console.log("ADDING: "+el);
-                        queueParser.push({
-                            idx: ij,
-                            url: el
-                        });
-                        newFiles.push({
-                            uri: el
+                    if (['soundcloud.com', 'youtube.com', 'vimeo.com'].includes(client.host)) {
+                        var relatives = client.links.relative;
+                        relatives.forEach((el, ij) => {
+                            el = 'https://' + client.host + el;
+
+                            if (noDupes.indexOf(el) == -1 && linker.isSupportedLink(el)) {
+
+                                queueParser.push({
+                                    idx: ij,
+                                    url: el
+                                });
+                                newFiles.push({
+                                    uri: el
+                                });
+                            }
+                            noDupes.push(el);
                         });
                     }
-                    noDupes.push(el);
+
+                    iframes.forEach((el, ij) => {
+
+                        el = linker.iframeToLink(el);
+
+                        if (noDupes.indexOf(el) == -1 && linker.isSupportedLink(el)) {
+                            queueParser.push({
+                                idx: ij,
+                                url: el
+                            });
+                            newFiles.push({
+                                uri: el
+                            });
+                        }
+                        noDupes.push(el);
+                    });
+                    resolve([newFiles, queueParser]);
+
                 });
+
+                client.on("error", reject);
+
+                client.fetch();
+
+            } else {
+                PlayerActions.addPlaylist([{
+                    uri: parsed.url,
+                    title: parsed.title
+                }]);
             }
-
-            iframes.forEach( (el, ij) => {
-
-                el = linker.iframeToLink(el);
-
-                if (noDupes.indexOf(el) == -1 && linker.isSupportedLink(el)) {
-                    queueParser.push({
-                        idx: ij,
-                        url: el
-                    });
-                    newFiles.push({
-                        uri: el
-                    });
-                }
-                noDupes.push(el);
-            });
-
-            cb(newFiles, queueParser);
-
         });
-
-        client.on("error", function(err){
-            cb([], []);
-        });
-
-        client.fetch();
-
-    } else {
-        PlayerActions.addPlaylist([{
-            uri: parsed.url,
-            title: parsed.title
-        }]);
     }
-};
+}
 
-module.exports = linker;
+export
+default supportedLinks;
