5e1c593fdb4a9c79ed578b96e0a315398841207c
Alexandru Branza <branza.alex@gmail.com>
2017-06-04 14:25:03 +0300

    Update isSupported.js

--- src/utils/isSupported.js
+++ src/utils/isSupported.js
@@ -1,7 +1,7 @@
 
 var supported = {
     ext: {
-        video: ['.mkv', '.avi', '.mp4', '.m4v', '.mpg', '.mpeg', '.webm', '.flv', '.ogg', '.ogv', '.mov', '.wmv', '.3gp', '.3g2', '.m2v', '.real'],
+        video: ['.mkv', '.avi', '.mp4', '.m4v', '.mpg', '.mpeg', '.webm', '.flv', '.ogg', '.ogv', '.mov', '.wmv', '.3gp', '.3g2', '.m2v', '.real', '.m2ts'],
         audio: ['.m4a', '.mp3', '.flac', '.wma', '.wav'],
         subs: ['.sub', '.srt', '.vtt'],
         torrent: ['.torrent', '.magnet']
