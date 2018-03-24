0bf7282fe315101c6338fd0f16bc8dbea6b35529
Alexandru Branza <branza.alex@gmail.com>
2018-03-24 14:28:08 +0200

    Delay Update Check by 3 sec

--- src/components/Player/utils/updates.js
+++ src/components/Player/utils/updates.js
@@ -9,37 +9,39 @@ import {
 
 module.exports = {
     checkUpdates: () => {
-        ls('version', '1.20');
-        if (!ls.isSet('updateCheck')) ls('updateCheck', 0);
-        
-        // announce update every 3 days
-        if (ls('updateCheck') < Math.floor(Date.now() / 1000) - 259200) {
-            needle.get('http://powder.media/version', (err, res) => {
-                if (!err && res.body && res.body.length < 100 && res.body.includes && res.body.includes('|')) {
-                    var vers = new TextDecoder("utf-8").decode(res.body).split('|');
-                    if (vers[0] == ls('version')) {
-                        ls('updateCheck', Math.floor(Date.now() / 1000));
-                    } else {
-//                        var iconPath = path.join(__dirname, '../../../../images/icons/powder-icon-padding.png');
-//                        if (!fs.accessSync(iconPath, fs.F_OK))
-//                            iconPath = path.join(__dirname, '../../../../../images/icons/powder-icon-padding.png');
+        setTimeout(function() {
+            ls('version', '1.20');
+            if (!ls.isSet('updateCheck')) ls('updateCheck', 0);
+            
+            // announce update every 3 days
+            if (ls('updateCheck') < Math.floor(Date.now() / 1000) - 259200 && !window.noUpdate) {
+                needle.get('http://powder.media/version', (err, res) => {
+                    if (!err && res.body && res.body.length < 100 && res.body.includes && res.body.includes('|')) {
+                        var vers = new TextDecoder("utf-8").decode(res.body).split('|');
+                        if (vers[0] == ls('version')) {
+                            ls('updateCheck', Math.floor(Date.now() / 1000));
+                        } else {
+    //                        var iconPath = path.join(__dirname, '../../../../images/icons/powder-icon-padding.png');
+    //                        if (!fs.accessSync(iconPath, fs.F_OK))
+    //                            iconPath = path.join(__dirname, '../../../../../images/icons/powder-icon-padding.png');
 
-                        notifier.notify({
-                            title: 'Powder v' + vers[0] + ' Available',
-                            message: 'Includes awesome new features!',
-//                            icon: iconPath,
-                            sound: true,
-                            wait: true
-                        }, (err, response) => {
-                            if (!err) {
-                                ls('updateCheck', Math.floor(Date.now() / 1000));
-                                if (response == 'activate')
-                                    shell.openExternal(vers[1]);
-                            }
-                        });
+                            notifier.notify({
+                                title: 'Powder v' + vers[0] + ' Available',
+                                message: 'Includes awesome new features!',
+    //                            icon: iconPath,
+                                sound: true,
+                                wait: true
+                            }, (err, response) => {
+                                if (!err) {
+                                    ls('updateCheck', Math.floor(Date.now() / 1000));
+                                    if (response == 'activate')
+                                        shell.openExternal(vers[1]);
+                                }
+                            });
+                        }
                     }
-                }
-            });
-        }
+                });
+            }
+        }, 3000)
     }
 }
