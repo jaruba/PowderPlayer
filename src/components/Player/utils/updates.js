a81bcdf883bad3421601d1dc0a2cada5b1e87476
Alexandru Branza <branza.alex@gmail.com>
2019-09-05 09:36:11 +0300

    Update Link to Latest Version

--- src/components/Player/utils/updates.js
+++ src/components/Player/utils/updates.js
@@ -15,7 +15,7 @@ module.exports = {
             
             // announce update every 3 days
             if (ls('updateCheck') < Math.floor(Date.now() / 1000) - 259200 && !window.noUpdate) {
-                needle.get('http://powder.media/version', (err, res) => {
+                needle.get('https://powder.media/version', (err, res) => {
                     if (!err && res.body && res.body.length < 100 && res.body.includes && res.body.includes('|')) {
                         var vers = new TextDecoder("utf-8").decode(res.body).split('|');
                         if (vers[0] == ls('version')) {
