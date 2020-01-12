ffd8dbe72af8707ea314cf46be18b209f1722952
Alexandru Branza <branza.alex@gmail.com>
2020-01-12 20:22:43 +0200

    Add Web API

--- src/utils/clArgs.js
+++ src/utils/clArgs.js
@@ -14,6 +14,106 @@ var getParam = (el, arg) => {
     return el.replace(arg + '=', '').split('"').join('');
 }
 
+function startWebApi(el) {
+    const getPort = require('get-port')
+    getPort({ port: (el.includes('=') ? parseInt(el.split('=')[1]) : 5090) }).then(port => {
+        const express = require('express')
+        const app = express()
+        const player = require('../components/Player/utils/player')
+
+        function err(msg) {
+            res.writeHead(500)
+            res.send(msg || 'api error')
+        }
+
+        app.get('/play', (req, res) => {
+            if (((player || {}).wcjs || {}).play) {
+                player.wcjs.play()
+                res.send('success')
+            } else err()
+        })
+
+        app.get('/pause', (req, res) => {
+            if (((player || {}).wcjs || {}).pause) {
+                player.wcjs.pause()
+                res.send('success')
+            } else err()
+        })
+
+        app.get('/togglePause', (req, res) => {
+            if (((player || {}).wcjs || {}).togglePause) {
+                player.wcjs.togglePause()
+                res.send('success')
+            } else err()
+        })
+
+        app.get('/next', (req, res) => {
+            if ((player || {}).next) {
+                player.next()
+                res.send('success')
+            } else err()
+        })
+
+        app.get('/prev', (req, res) => {
+            if ((player || {}).prev) {
+                player.prev()
+                res.send('success')
+            } else err()
+        })
+
+        app.get('/item_data', (req, res) => {
+            if ((player || {}).itemDesc && ((((player || {}).wcjs || {}).playlist || {}).items || [])[0]) {
+
+                res.setHeader('Content-Type', 'application/json; charset=utf-8')
+
+                res.send(JSON.stringify(player.itemDesc()))
+
+            } else err()
+        })
+
+        app.get('/playlist', (req, res) => {
+            if (((((player || {}).wcjs || {}).playlist || {}).items || [])[0]) {
+                var items = []
+
+                for (i = 0; i < player.wcjs.playlist.items.count; i++)
+                    items.push(player.itemDesc(i))
+
+                res.setHeader('Content-Type', 'application/json; charset=utf-8')
+
+                res.send(JSON.stringify(items))
+            } else err()
+        })
+
+        app.get('/add_playlist', (req, res) => {
+            if (req.query.url) {
+
+                window.playerDrop({ preventDefault: () => {}, dataTransfer: { files: [], getData: function() { return req.query.url } } })
+
+                res.send('success')
+
+            } else if (req.query.file) {
+
+                var files = []
+
+                files.push({
+                    path: req.query.file,
+                    name: req.query.file.replace(/^.*[\\\/]/, '')
+                })
+
+                window.playerDrop({ dataTransfer: { files: files }, preventDefault: () => {} })
+
+                res.send('success')
+
+            } else err()
+        })
+
+        app.listen(port, () => console.log(`Web API listening on port ${port}!`))
+    }).catch(err => {
+        console.log('Error: Could not start Web API')
+        console.error(err)
+    })
+}
+
 module.exports = {
     process: (args) => {
         if (args.length) {
@@ -38,6 +138,8 @@ module.exports = {
                             window.extendedTitle = window.clTitle
                         }
                         window.getExtendedDetails = true
+                    } else if (el.startsWith('--web-api')) {
+                        startWebApi(el)
                     }
                 } else {
                     if (path.isAbsolute(el)) {
